50df9b2a3947cdec8707af55d4e23cb6d43be918
teilon <teiloncw@gmail.com>
2018-02-07 11:24:18 +0300

    all right

--- app_docktor/settings.py
+++ app_docktor/settings.py
@@ -119,4 +119,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = os.path.join(BASE_DIR, '/static/')
+
+MEDIA_ROOT = os.path.join(BASE_DIR, '/media/')
